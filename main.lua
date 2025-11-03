fcf5bb4a077f34f9e6ec93450a112c794a1a8198
licalsinj <licalsinj@gmail.com>
2025-11-02 18:52:53 -0600

    Got buckets to show up.

--- main.lua
+++ main.lua
@@ -1,22 +1,27 @@
--- a simple one button game where you sort colored balls
+-- A simple one button game where you sort colored balls
 
 function love.load()
+	-- Global Constants
 	MAX_SPEED = 30
 	BALL_RADIUS = 20
+	-- TODO: Make these actually be used to create the screen
+	FRAME_WIDTH = 800
+	FRAME_HEIGHT = 600
+
+	-- Global Variables
 	Directions = { -1, 1 }
+	Colors = {
+		{ 255, 0, 0 },
+		{ 0, 0, 255 },
+	}
+	Speed = 1
 	Ball = {
 		x = 350,
 		y = 30,
-		speed = 1,
+		speed = Speed,
 		direction = Directions[love.math.random(2)],
 		drop = false,
-		color_index = love.math.random(1, 3),
-	}
-	Speed = 3
-	Colors = {
-		{ 255, 0, 0 },
-		{ 0, 255, 0 },
-		{ 0, 0, 255 },
+		color_index = love.math.random(1, #Colors),
 	}
 end
 
@@ -33,13 +38,16 @@ function love.update(dt)
 			if Speed < MAX_SPEED then
 				Speed = Speed + 1
 			end
+			if Speed == MAX_SPEED / 2 and #Colors < 3 then
+				table.insert(Colors, { 0, 255, 0 })
+			end
 			Ball = {
 				x = 350,
 				y = 30,
 				speed = Speed,
 				direction = Directions[love.math.random(2)],
 				drop = false,
-				color_index = math.random(1, 3),
+				color_index = math.random(1, #Colors),
 			}
 		end
 	-- TODO: Make this a global setting when you figure out how to setup screen sizes
@@ -50,6 +58,14 @@ function love.update(dt)
 end
 
 function love.draw()
+	local color_count = #Colors
+	for index, value in ipairs(Colors) do
+		local width = FRAME_WIDTH / color_count
+		local height = 100
+		love.graphics.setColor(value)
+		love.graphics.rectangle("fill", (index - 1) * width, FRAME_HEIGHT - height, width, height)
+	end
+
 	love.graphics.setColor(Colors[Ball.color_index])
 	love.graphics.circle("fill", Ball.x, Ball.y, BALL_RADIUS)
 end
