b50fce9ea88578c4c47ef3dc7eb8e39e8cdd2c94
licalsinj <licalsinj@gmail.com>
2025-11-08 22:03:39 -0600

    I guess I built a sythesiser...

--- main.lua
+++ main.lua
@@ -72,49 +72,51 @@ function love.load()
 	Ball = reset_ball()
 
 	-- Sound Stuff
-	Border_Sound = love.audio.newSource("sound_effects/border.ogg", "static")
-	C_Sound = love.audio.newSource("sound_effects/C.ogg", "static")
+	C_Sound = love.audio.newSource("sound_effects/border.ogg", "static")
+	Border_Sound = love.audio.newSource("sound_effects/C.ogg", "static")
 	Pitch_Index = 1
 	local base_pitch = 0.5
 	local step = 1 / 12
 	Pitch_List = {
 		base_pitch, -- C  1
-		(base_pitch + base_pitch * step), -- C# 2
-		(base_pitch + 2 * base_pitch * step), -- D  3
-		(base_pitch + 3 * base_pitch * step), -- D# 4
-		(base_pitch + 4 * base_pitch * step), -- E  5
-		(base_pitch + 5 * base_pitch * step), -- F  6
-		(base_pitch + 6 * base_pitch * step), -- F# 7
-		(base_pitch + 7 * base_pitch * step), -- G  8
-		(base_pitch + 8 * base_pitch * step), -- G# 9
-		(base_pitch + 9 * base_pitch * step), -- A  10
-		(base_pitch + 10 * base_pitch * step), -- A# 11
-		(base_pitch + 11 * base_pitch * step), -- B  12
+		(base_pitch + base_pitch * step) - 0.0125, -- C# 2
+		(base_pitch + 2 * base_pitch * step) - 0.02375, -- D  3
+		(base_pitch + 3 * base_pitch * step) - 0.0325, -- D# 4
+		(base_pitch + 3 * base_pitch * step) + 0.0025, -- E 5
+		(base_pitch + 4 * base_pitch * step), -- F
+		(base_pitch + 5 * base_pitch * step), -- F#
+		(base_pitch + 6 * base_pitch * step), -- G
+		(base_pitch + 7 * base_pitch * step), -- G#
+		(base_pitch + 9 * base_pitch * step) - 0.03625, -- A  10
+		(base_pitch + 10 * base_pitch * step) - 0.0275, -- A# 11
+		(base_pitch + 11 * base_pitch * step) - 0.016525, -- B  12
 		2 * base_pitch, -- C  13
-		(2 * base_pitch + base_pitch * step),
-		(2 * base_pitch + 2 * 2 * base_pitch * step),
-		(2 * base_pitch + 3 * 2 * base_pitch * step),
-		(2 * base_pitch + 4 * 2 * base_pitch * step),
-		(2 * base_pitch + 5 * 2 * base_pitch * step),
-		(2 * base_pitch + 6 * 2 * base_pitch * step),
-		(2 * base_pitch + 7 * 2 * base_pitch * step),
-		(2 * base_pitch + 8 * 2 * base_pitch * step),
-		(2 * base_pitch + 9 * 2 * base_pitch * step),
-		(2 * base_pitch + 10 * 2 * base_pitch * step),
-		(2 * base_pitch + 11 * 2 * base_pitch * step),
+		(2 * base_pitch + base_pitch * step) + 0.0025 * 5.5, -- C# 2
+		(2 * base_pitch + 2 * 2 * base_pitch * step) - 0.005 * 9, -- D  3,
+		(2 * base_pitch + 3 * 2 * base_pitch * step) - 0.0025 * 25, -- D# 4,
+		(2 * base_pitch + 3 * 2 * base_pitch * step) + 0.0075, -- E
+		(2 * base_pitch + 4 * 2 * base_pitch * step), -- F
+		(2 * base_pitch + 5 * 2 * base_pitch * step) - 0.0025 * 3, -- F#
+		(2 * base_pitch + 6 * 2 * base_pitch * step) - 0.0025 * 3, -- G
+		(2 * base_pitch + 7 * 2 * base_pitch * step), -- G#
+		(2 * base_pitch + 9 * 2 * base_pitch * step) - 0.0025 * 28, -- A
+		(2 * base_pitch + 10 * 2 * base_pitch * step) - 0.0025 * 22, -- A#
+		(2 * base_pitch + 11 * 2 * base_pitch * step) - 0.0025 * 12, -- B
 		4 * base_pitch,
 	}
 	-- Pitch_Shift = 0.25
-	for i, v in ipairs(Pitch_List) do
-		print("index: " .. i .. " value: " .. v)
-	end
 	Major_Scale = { 1, 3, 5, 6, 8, 10, 12, 13 }
 	Minor_Scale = { 1, 3, 4, 6, 8, 9, 11, 13 }
 	Blues_Scale = { 1, 4, 6, 7, 8, 11, 12, 13 } -- minor blues scale
-	Current_Scale = Minor_Scale
+	Cromatic_Scale = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 }
+	-- Current_Scale = { 8, 8, 8 }
+	Current_Scale = Blues_Scale
 	Scale_Index = 1
 	Scale_Direction = 1
-	Octave = 2
+	Octave = 1
+	Repeat = 1
+	Repeat_Count = 1
+	Pitch_Adjustment = -0.0025 * 0
 end
 
 function love.update(dt)
@@ -124,13 +126,18 @@ function love.update(dt)
 	if math.floor(timer) % 50 == 0 then
 		print("Scale_Index: " .. Scale_Index)
 		love.audio.stop()
-		C_Sound:setPitch(Pitch_List[Current_Scale[Scale_Index] + ((Octave - 1) * 12)])
+		C_Sound:setPitch(Pitch_List[Current_Scale[Scale_Index] + ((Octave - 1) * 12)] + Pitch_Adjustment)
 		print("getPitch: " .. C_Sound:getPitch())
 		love.audio.play(C_Sound)
-		Scale_Index = Scale_Index + Scale_Direction
-		if Scale_Index > #Current_Scale or Scale_Index == 0 then
-			Scale_Direction = Scale_Direction * -1
-			Scale_Index = Scale_Index + Scale_Direction * 2
+		if Repeat < Repeat_Count then
+			Repeat = Repeat + 1
+		else
+			Repeat = 1
+			Scale_Index = Scale_Index + Scale_Direction
+			if Scale_Index > #Current_Scale or Scale_Index == 0 then
+				Scale_Direction = Scale_Direction * -1
+				Scale_Index = Scale_Index + Scale_Direction * 2
+			end
 		end
 	end
 	-- Function to Auto Play the game for testing
